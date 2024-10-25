ff30db443bccc1b369a81ed1928944006176056a
Blizek <blazej.naziemiec@gmail.com>
2024-10-25 16:26:00 +0200

    feat: creating object 'Vector2d'

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -4,20 +4,6 @@ import agh.ics.oop.model.MoveDirection;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("Start");
-        MoveDirection[] parsedDirections = OptionParser.parseStringToMoveDirections(args);
-        run(parsedDirections);
-        System.out.println("Stop");
-    }
 
-    private static void run(MoveDirection[] directions) {
-        for (MoveDirection direction : directions) {
-            switch (direction) {
-                case FORWARD -> System.out.println("Zwierzak idzie do przodu");
-                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
-                case RIGHT -> System.out.println("Zwierzak skręca w prawo");
-                case LEFT -> System.out.println("Zwierzak skręca w lewo");
-            }
-        }
     }
 }
