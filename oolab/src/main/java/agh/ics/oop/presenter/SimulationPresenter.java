dd1f20ff2ee013899be6fd3e49038365e68d79b6
Blizek <blazej.naziemiec@gmail.com>
2024-12-14 17:04:40 +0100

    feat: działająca symulacja z widokiem

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -6,25 +6,79 @@ import agh.ics.oop.SimulationEngine;
 import agh.ics.oop.model.*;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimulationPresenter implements MapChangeListener {
     private WorldMap worldMap;
+    private Vector2d lowerLeft;
+    private Vector2d upperRight;
     @FXML
     private Label infoLabel;
     @FXML
-    private TextField textField;
+    private TextField animalsMovesTextField;
+    @FXML
+    private GridPane mapGrid;
 
     public void setWorldMap(WorldMap map) {
         this.worldMap = map;
     }
 
-    public void drawMap() {
+    private void clearGrid() {
+        mapGrid.getChildren().retainAll(mapGrid.getChildren().get(0)); // hack to retain visible grid lines
+        mapGrid.getColumnConstraints().clear();
+        mapGrid.getRowConstraints().clear();
+    }
 
+    public void drawMap() {
+        clearGrid();
+        Boundary mapBoundary = worldMap.getCurrentBounds();
+        lowerLeft = mapBoundary.lowerLeft();
+        upperRight = mapBoundary.upperRight();
+
+        // GridPane.setHalignment(label, HPos.CENTER);
+        for (int x = lowerLeft.getX(); x <= upperRight.getX() + 1; x++) {
+            mapGrid.getColumnConstraints().add(new ColumnConstraints(35));
+        }
+        // Create a new grid based on the current dimensions of the map
+        for (int y = lowerLeft.getY(); y <= upperRight.getY() + 1; y++) {
+            mapGrid.getRowConstraints().add(new RowConstraints(35));
+        }
+
+        Label label = new Label("y/x");
+        mapGrid.add(label, 0, 0);
+        GridPane.setHalignment(label, HPos.CENTER);
+
+        for (int x = lowerLeft.getX(); x <= upperRight.getX(); x++) {
+            label = new Label(String.valueOf(x));
+            mapGrid.add(label, x - lowerLeft.getX() + 1, 0);
+            GridPane.setHalignment(label, HPos.CENTER);
+        }
+
+        for (int y = upperRight.getY(); y >= lowerLeft.getY(); y--) {
+            label = new Label(String.valueOf(y));
+            mapGrid.add(label, 0, upperRight.getY() - y + 1);
+            GridPane.setHalignment(label, HPos.CENTER);
+        }
+
+        for (int x = lowerLeft.getX(); x <= upperRight.getX(); x++) {
+            for (int y = upperRight.getY(); y >= lowerLeft.getY(); y--) {
+                Vector2d currentPosition = new Vector2d(x, y);
+                WorldElement elementAtPosition = worldMap.objectAt(currentPosition);
+                if (elementAtPosition != null) {
+                    label = new Label(elementAtPosition.toString());
+                    mapGrid.add(label, x - lowerLeft.getX() + 1, upperRight.getY() - y + 1);
+                    GridPane.setHalignment(label, HPos.CENTER);
+                }
+            }
+        }
     }
 
     @Override
@@ -37,7 +91,7 @@ public class SimulationPresenter implements MapChangeListener {
 
     public void onSimulationStartClicked() {
         try {
-            String animalsMoveString = textField.getText();
+            String animalsMoveString = animalsMovesTextField.getText();
             List<MoveDirection> animalsMovesAsList = OptionsParser.parseStringToMoveDirections(animalsMoveString.split(" "));
 
             Vector2d position1 = new Vector2d(2, 2);
